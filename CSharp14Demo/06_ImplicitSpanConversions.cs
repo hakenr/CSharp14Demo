2ea1be2caf380c757008fe1c00f031b35c560f99
Robert Haken <haken@havit.cz>
2025-05-20 23:56:15 +0200

    Implicit Span conversions

---
+++ CSharp14Demo/06_ImplicitSpanConversions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haken.CSharp14Demo.ImplicitSpanConversions;
+
+public static class MyExtensions
+{
+	public static bool HasSingle<T>(this ReadOnlySpan<T> span, T value) { return true; }
+}
+
+public class  Demo
+{
+	public static void Run()
+	{
+		ReadOnlySpan<char> myReadOnlySpan = stackalloc char[10];
+		Span<char> mySpan = stackalloc char[10];
+		char[] myArray = new char[10];
+		string someType = "Test";
+
+		myReadOnlySpan.HasSingle('x'); // just works
+
+		mySpan.HasSingle('x');   // C# 14 
+		myArray.HasSingle('x');  // C# 14
+		someType.HasSingle('x');   // C# 14 if there is an implicit conversion operator to Span/ReadOnlySpan/T[]
+	}
+
+	
+}
