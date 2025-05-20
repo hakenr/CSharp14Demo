2eabe3cad64c1beb867659de25b7e70bde73108c
Robert Haken <haken@havit.cz>
2025-05-21 00:03:19 +0200

    LambdaParametersModifiers

---
+++ CSharp14Demo/07_LambdaParametersModifiers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haken.CSharp14Demo.LambdaParametersModifiers;
+
+public class Demo
+{
+	delegate bool TryParse<T>(string text, out T result);
+
+	public void Run()
+	{
+		// C# 13
+		TryParse<int> parse1 = (string text, out int result) => Int32.TryParse(text, out result);
+
+		// C# 14 - parameter modifiers no longer require specifying the parameter type
+		TryParse<int> parse2 = (text, out result) => Int32.TryParse(text, out result);
+	}
+}
