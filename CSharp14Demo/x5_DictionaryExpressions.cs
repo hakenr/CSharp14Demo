9eb30800b45364fc8729eeef8b37cb6c47b93a8b
Robert Haken <haken@havit.cz>
2025-05-21 01:12:31 +0200

    Dictionary expressions

---
+++ CSharp14Demo/x5_DictionaryExpressions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haken.CSharp14Demo.DictionaryExpressions;
+
+public class Demo
+{
+	public void Run()
+	{
+		Dictionary<string, int> currentStudents = ["joe" : 1, "jane" : 2, "john" : 3];
+		Dictionary<string, int> newStudents = [.. currentStudents, "mds" : 5];
+	}	
+}
+
+
+// https://github.com/dotnet/csharplang/issues/8659
+// https://github.com/dotnet/csharplang/blob/main/proposals/dictionary-expressions.md
