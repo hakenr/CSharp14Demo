f83ffd0a13f327c7ff8c1fe55e2a9c1535ec65b0
Robert Haken <haken@havit.cz>
2025-05-20 23:30:59 +0200

    Partial members - constructors, events

---
+++ CSharp14Demo/04_PartialMembers_Ctor_Event.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haken.CSharp14Demo.PartialMembers;
+
+public partial class Demo
+{
+	// partial constructor
+	public partial Demo();
+
+	// partial event
+	public partial event EventHandler MyEvent;
+}
+
+
+// usually source generated
+public partial class Demo
+{
+	public partial Demo()
+	{
+		Console.WriteLine("Partial constructor called.");
+	}
+
+	public partial event EventHandler MyEvent
+	{
+		add
+		{
+			Console.WriteLine("Adding event handler.");
+			_myEvent += value;
+		}
+		remove
+		{
+			Console.WriteLine("Removing event handler.");
+			_myEvent -= value;
+		}
+	}
+	private EventHandler _myEvent;
+}
