b5f879740a9ac985a3b47c024e99ffb490ddd348
Robert Haken <haken@havit.cz>
2025-05-20 23:41:28 +0200

    Null-conditional assignment

---
+++ CSharp14Demo/05_NullConditionalAssignment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haken.CSharp14Demo.NullConditionalAssignment;
+
+public class Demo
+{
+	private Person _person;
+
+	public void Run()
+	{
+		// C# 13
+		if (_person != null)
+		{
+			_person.Name = "John Doe";
+		}
+
+		// C# 14 - Null-conditional assignment
+		_person?.Name = "John Doe";
+		_person?.NameChanged += (s, e) => Console.WriteLine("Name changed!");
+
+		// not allowed in C# 14 (yet?)
+		// _person?.Age++;
+
+		// in discussion for C# vNext
+		// await? person?.DoSomethingAsync();
+	}
+
+
+
+	private record Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public event EventHandler NameChanged;
+	}
+}
+
+
+
